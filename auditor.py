64ab46b0ee4b2304b82bf37516135cf777e3fce5
Andrew Sow <2601569@sit.singaporetech.edu.sg>
2026-09-10 22:26:18 +0800

    First commit

---
+++ auditor.py
@@ -0,0 +1,8 @@
+inventory = 0
+
+if not quit:
+    stockQuant = input("Enter a stock quantity")
+    
+while True:
+    stockQuant = input("Enter a stock quantity: ")
+    if stockQuant == "quit": break
