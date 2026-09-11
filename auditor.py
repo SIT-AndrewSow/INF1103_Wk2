198669dcc18841f8cac35fc0dcdae4b07b9c76e0
Andrew Sow <2601569@sit.singaporetech.edu.sg>
2026-09-11 15:02:12 +0800

    Add overstock alert, reporting

--- auditor.py
+++ auditor.py
@@ -1,4 +1,5 @@
 inventory = 0
+failedEntries = 0
 
 if not quit:
     stockQuant = input("Enter a stock quantity")
@@ -6,14 +7,20 @@ if not quit:
 while True:
     stockQuant = input("Enter a stock quantity: ")
     
+    # check if the inventory limit has been exceeded
+    if inventory > 500:
+        print("Inventory limit exceeded. Cannot add more stock.")
+        break
+    
     # check if the user wants to quit
     if stockQuant == "quit": 
-        print("Final inventory:", inventory)
+        print(f"Total Units Processed:{inventory} and the number of Failed/Rejected Entries:{failedEntries}.")
         break
     
     # check if input is a valid number, isdigit checks for negative sign and returns false
     if not stockQuant.isdigit():
         print("Invalid input. Please enter a valid positive number.")
+        failedEntries += 1
     else:
         stockQuant = int(stockQuant)
         inventory += stockQuant
